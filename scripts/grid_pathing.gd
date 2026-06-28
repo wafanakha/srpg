4698e83b08e4826f57b9803bfc1e0bc10f7be0af
wafanakha <wafanakha15@gmail.com>
2026-06-28 23:14:19 +0700

    add highlighting + turn base

--- scripts/grid_pathing.gd
+++ scripts/grid_pathing.gd
@@ -3,15 +3,20 @@ extends Node2D
 var astar_grid : AStarGrid2D
 @onready var tilemap = $ground_1 
 @onready var warrior = $warrior
+@onready var lancer = $lancer
+@onready var highlight_layer = $highlight
 
 func _ready():
 	warrior.play("warrior_idle")
+	lancer.play("idle")
+	
 	astar_grid = AStarGrid2D.new()
 	astar_grid.region = tilemap.get_used_rect()
-	astar_grid.cell_size = Vector2(64, 64) 
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.cell_size = Vector2(64, 64) 
 	astar_grid.update()
 	
+	draw_movement_range()
 
 	for x in range(astar_grid.region.position.x, astar_grid.region.end.x):
 		for y in range(astar_grid.region.position.y, astar_grid.region.end.y):
@@ -27,19 +32,37 @@ func _unhandled_input(event):
 		
 		var mouse_pos = get_global_mouse_position()
 		var target_grid_pos = tilemap.local_to_map(mouse_pos)
-		
 		var warrior_grid_pos = tilemap.local_to_map(warrior.global_position)
 		
 		var path = astar_grid.get_id_path(warrior_grid_pos, target_grid_pos)
-		
-		if path.is_empty():
+		if path.is_empty() and path :
 			print("no tiles")
+		
+		elif (path.size() - 1) > warrior.mov_range:
+			print(path.size())
 		else:
 			path.pop_front()
 			var tween = get_tree().create_tween()
 			warrior.play("warrior_run")
+			
 			for point in path:
 				var target_pixel_pos = tilemap.map_to_local(point)
 				tween.tween_property(warrior, "global_position", target_pixel_pos, 0.4)
 			await tween.finished
 			warrior.play("warrior_idle")
+			draw_movement_range()
+			
+func draw_movement_range():
+	highlight_layer.clear()
+	var start_pos = tilemap.local_to_map(warrior.global_position)
+	var range_limit = warrior.mov_range
+	
+	print_debug(start_pos)
+	for x in range(-range_limit, range_limit + 1):
+		for y in range(-range_limit, range_limit + 1):
+			var target_pos = start_pos + Vector2i(x,y)
+			var path = astar_grid.get_id_path(start_pos, target_pos)
+			print(target_pos)
+			if not path.is_empty() and (path.size() - 1) <= range_limit:
+				highlight_layer.set_cell(target_pos, 1, Vector2i(0,0))
+	
---
+++ scripts/unit.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+
+@export var mov_range = 4
+@export var attack = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
