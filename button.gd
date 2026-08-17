6d567666eb7be1277d5d9c22a69b4d886d977df0
wafanakha <wafanakha15@gmail.com>
2026-08-18 00:43:57 +0700

    add turn order

--- button.gd
+++ button.gd
@@ -3,6 +3,7 @@ extends Button
 @onready var normal_button = preload("res://Tiny Swords (Free Pack)/UI Elements/UI Elements/Buttons/SmallBlueRoundButton_Regular.png")
 @onready var pressed_button = preload("res://Tiny Swords (Free Pack)/UI Elements/UI Elements/Buttons/SmallBlueRoundButton_Pressed.png")
 
+signal end_turn()
 var down = true
 
 func _ready() -> void:
@@ -17,7 +18,7 @@ func _process(delta: float) -> void:
 
 func _on_button_down() -> void:
 	icon = pressed_button
-
+	emit_signal("end_turn")
 	pass 
 
 
--- scripts/grid_pathing.gd
+++ scripts/grid_pathing.gd
@@ -3,15 +3,18 @@ extends Node2D
 var current_unit
 var astar_grid : AStarGrid2D
 
-var ismoving = false
 @onready var tilemap = $ground_1 
 @onready var warrior = $warrior
 @onready var lancer = $lancer
 @onready var highlight_layer = $highlight
+@onready var change_trigger = $Control/Button
+var is_moved = false
 
 func _ready():
 	warrior.play("idle")
 	lancer.play("idle")
+	
+	change_trigger.end_turn.connect(change_unit)
 	
 	astar_grid = AStarGrid2D.new()
 	astar_grid.region = tilemap.get_used_rect()
@@ -48,11 +51,13 @@ func _unhandled_input(event):
 		var enemies = lancer if current_unit == warrior else warrior
 		var enemies_pos = tilemap.local_to_map(enemies.global_position)
 		
+		var full_attack_range = current_unit.mov_range + current_unit.attack_range
+		
 		# CEK ATK
-		if target_grid_pos == enemies_pos :
+		if target_grid_pos == enemies_pos:
 			var distance = abs(current_unit_grid_pos.x - target_grid_pos.x) + abs(current_unit_grid_pos.y - target_grid_pos.y)
 			
-			if distance == 1 :
+			if distance <= full_attack_range:
 				if target_grid_pos.x > current_unit_grid_pos.x :
 					current_unit.flip_h = false
 				elif target_grid_pos.x < current_unit_grid_pos.x :
@@ -62,33 +67,33 @@ func _unhandled_input(event):
 				await current_unit.animation_finished
 				current_unit.play("idle")
 				
-				if current_unit == warrior:
-					current_unit = lancer
-				else :
-					current_unit = warrior
-					
-				draw_movement_range()
+				change_unit()
 				
 			else:
-				print("Kejauhan! Gagal nyerang.")
+				print("too far to attack")
 				
 			return 
 			
-		# MOVE
+		
 		
 		if target_grid_pos == current_unit_grid_pos:
 			return
-			
+		
+		# MOVE
 		astar_grid.set_point_solid(current_unit_grid_pos, false)
 		var path = astar_grid.get_id_path(current_unit_grid_pos, target_grid_pos)
 		
 		if path.is_empty():
-			print("Tidak bisa lewat sana!")
+			print("no!")
 			astar_grid.set_point_solid(current_unit_grid_pos, true) 
 			
 		elif (path.size() - 1) > current_unit.mov_range:
-			print("Kejauhan!")
+			print("too far")
 			astar_grid.set_point_solid(current_unit_grid_pos, true) 
+		elif is_moved:
+			print("was moved")
+			astar_grid.set_point_solid(current_unit_grid_pos, true) 
+			
 		else:
 			path.pop_front()
 			var tween = get_tree().create_tween()
@@ -109,25 +114,68 @@ func _unhandled_input(event):
 			astar_grid.set_point_solid(target_grid_pos, true)
 			current_unit.play("idle")
 			
-			if current_unit == warrior:
-				current_unit = lancer
-			else :
-				current_unit = warrior
-				
+			is_moved = !is_moved
 			draw_movement_range()
+
 			
 func draw_movement_range():
 	highlight_layer.clear()
 	var start_pos = tilemap.local_to_map(current_unit.global_position)
 	var range_limit = current_unit.mov_range
+	var attack_range = current_unit.attack_range
+	var mov_range = []
+	var atk_range = []
 	
 	astar_grid.set_point_solid(start_pos, false)
 	for x in range(-range_limit, range_limit + 1):
 		for y in range(-range_limit, range_limit + 1):
 			var target_pos = start_pos + Vector2i(x,y)
 			var path = astar_grid.get_id_path(start_pos, target_pos)
+			
 			if not path.is_empty() and (path.size() - 1) <= range_limit:
-				highlight_layer.set_cell(target_pos, 1, Vector2i(0,0))
+				mov_range.append(target_pos)
 	
+	if !is_moved:
+		for move_pos in mov_range:
+			for x in range(-attack_range, attack_range + 1):
+				for y in range(-attack_range, attack_range + 1):
+					var distance = abs(x) + abs(y)
+
+					if distance == attack_range:
+						var atk_pos = move_pos + Vector2i(x,y)
+						
+						if not mov_range.has(atk_pos) and not atk_range.has(atk_pos):
+							atk_range.append(atk_pos)
+		
+		for pos in mov_range:
+			highlight_layer.set_cell(pos, 2, Vector2i(0,0))
+		for pos in atk_range:
+			highlight_layer.set_cell(pos, 3, Vector2i(0,0))
+	else:
+		for move_pos in mov_range:
+			for x in range(-attack_range, attack_range + 1):
+				for y in range(-attack_range, attack_range + 1):
+					var distance = abs(x) + abs(y)
+
+					if distance > 0 and distance <= attack_range:
+						var atk_pos = start_pos + Vector2i(x,y)
+						
+						if not atk_range.has(atk_pos):
+							atk_range.append(atk_pos)
+		
+		for pos in atk_range:
+			highlight_layer.set_cell(pos, 3, Vector2i(0,0))
+		
+			
 	astar_grid.set_point_solid(start_pos, true)
+
+func change_unit():
+	
+	if current_unit == warrior:
+		current_unit = lancer
+	else :
+		current_unit = warrior
+	
+	is_moved = !is_moved
+	draw_movement_range()
 	
