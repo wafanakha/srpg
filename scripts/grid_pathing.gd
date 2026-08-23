f6d78e512051a70307f31ac906fcf608b5d426a8
wafanakha <wafanakha15@gmail.com>
2026-08-23 14:20:59 +0700

    mengubah pergerakan dan serangan unit

--- scripts/grid_pathing.gd
+++ scripts/grid_pathing.gd
@@ -8,7 +8,6 @@ var astar_grid : AStarGrid2D
 @onready var lancer = $lancer
 @onready var highlight_layer = $highlight
 @onready var change_trigger = $Control/Button
-var is_moved = false
 
 func _ready():
 	warrior.play("idle")
@@ -38,7 +37,6 @@ func _ready():
 	astar_grid.set_point_solid(l_pos, true)
 	
 	current_unit = warrior
-	#draw_movement_range()
 
 
 func _unhandled_input(event):
@@ -51,49 +49,17 @@ func _unhandled_input(event):
 		var enemies = lancer if current_unit == warrior else warrior
 		var enemies_pos = tilemap.local_to_map(enemies.global_position)
 		
-		#var full_attack_range = current_unit.mov_range + current_unit.attack_range
-		var attack_range = current_unit.attack_range
-		
-		# CEK ATK
-		if target_grid_pos == enemies_pos:
-			var distance = abs(current_unit_grid_pos.x - target_grid_pos.x) + abs(current_unit_grid_pos.y - target_grid_pos.y)
-			
-			if distance <= attack_range:
-				if target_grid_pos.x > current_unit_grid_pos.x :
-					current_unit.flip_h = false
-				elif target_grid_pos.x < current_unit_grid_pos.x :
-					current_unit.flip_h = true
-				
-				current_unit.play("attack")
-				await current_unit.animation_finished
-				current_unit.play("idle")
-				
-				#change_unit()
-				
-			else:
-				print("too far to attack")
-				
-			return 
-			
-		
-		
 		if target_grid_pos == current_unit_grid_pos:
 			return
 		
-		# MOVE
 		astar_grid.set_point_solid(current_unit_grid_pos, false)
+		astar_grid.set_point_solid(enemies_pos, false)
+		
 		var path = astar_grid.get_id_path(current_unit_grid_pos, target_grid_pos)
 		
-		if path.is_empty():
-			print("no!")
+		if path.is_empty() and target_grid_pos != enemies_pos:
+			print("no ground detected!")
 			astar_grid.set_point_solid(current_unit_grid_pos, true) 
-			
-		#elif (path.size() - 1) > current_unit.mov_range:
-			#print("too far")
-			#astar_grid.set_point_solid(current_unit_grid_pos, true) 
-		#elif is_moved:
-			#print("was moved")
-			#astar_grid.set_point_solid(current_unit_grid_pos, true) 
 			
 		else:
 			path.pop_front()
@@ -104,71 +70,24 @@ func _unhandled_input(event):
 			elif target_grid_pos.x > current_unit_grid_pos.x:
 				current_unit.flip_h = false
 			
-			current_unit.play("run") 
+			current_unit.play("run")
 			
+			if target_grid_pos == enemies_pos:
+				path.pop_back()
+				
 			for point in path:
 				var target_pixel_pos = tilemap.map_to_local(point)
 				tween.tween_property(current_unit, "global_position", target_pixel_pos, 0.4)
 				
 			await tween.finished
 			
-			astar_grid.set_point_solid(target_grid_pos, true)
 			current_unit.play("idle")
+			if target_grid_pos == enemies_pos:
+				current_unit.play("attack")
+			astar_grid.set_point_solid(enemies_pos, true)
+			astar_grid.set_point_solid(target_grid_pos, true)
 			
-			is_moved = !is_moved
-			#draw_movement_range()
-
 			
-func draw_movement_range():
-	highlight_layer.clear()
-	var start_pos = tilemap.local_to_map(current_unit.global_position)
-	var range_limit = current_unit.mov_range
-	var attack_range = current_unit.attack_range
-	var mov_range = []
-	var atk_range = []
-	
-	astar_grid.set_point_solid(start_pos, false)
-	for x in range(-range_limit, range_limit + 1):
-		for y in range(-range_limit, range_limit + 1):
-			var target_pos = start_pos + Vector2i(x,y)
-			var path = astar_grid.get_id_path(start_pos, target_pos)
-			
-			if not path.is_empty() and (path.size() - 1) <= range_limit:
-				mov_range.append(target_pos)
-	
-	if !is_moved:
-		for move_pos in mov_range:
-			for x in range(-attack_range, attack_range + 1):
-				for y in range(-attack_range, attack_range + 1):
-					var distance = abs(x) + abs(y)
-
-					if distance == attack_range:
-						var atk_pos = move_pos + Vector2i(x,y)
-						
-						if not mov_range.has(atk_pos) and not atk_range.has(atk_pos):
-							atk_range.append(atk_pos)
-		
-		for pos in mov_range:
-			highlight_layer.set_cell(pos, 2, Vector2i(0,0))
-		for pos in atk_range:
-			highlight_layer.set_cell(pos, 3, Vector2i(0,0))
-	else:
-		for move_pos in mov_range:
-			for x in range(-attack_range, attack_range + 1):
-				for y in range(-attack_range, attack_range + 1):
-					var distance = abs(x) + abs(y)
-
-					if distance > 0 and distance <= attack_range:
-						var atk_pos = start_pos + Vector2i(x,y)
-						
-						if not atk_range.has(atk_pos):
-							atk_range.append(atk_pos)
-		
-		for pos in atk_range:
-			highlight_layer.set_cell(pos, 3, Vector2i(0,0))
-		
-			
-	astar_grid.set_point_solid(start_pos, true)
 
 func change_unit():
 	
@@ -176,7 +95,7 @@ func change_unit():
 		current_unit = lancer
 	else :
 		current_unit = warrior
-	
-	is_moved = !is_moved
-	draw_movement_range()
+
+func move_unit():
+	pass
 	
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,6 +1,5 @@
 extends AnimatedSprite2D
 
-@export var mov_range = 4
 @export var attack = 5
 @export var attack_range = 1
 @export var defense = 5
